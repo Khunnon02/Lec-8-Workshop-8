c0ad652867b98ec498d40f335ff5015de645e7b3
Khunnon02 <109897768+Khunnon02@users.noreply.github.com>
2022-10-09 20:51:07 +0700

    Add files via upload

---
+++ Workshop 8.php
@@ -0,0 +1,14 @@
+
+<body>
+    <form action="links.php" method="post" target="_blank">
+        username: <input type="text" name="username"><br>
+        password: <input type="text" name="password"><br>
+        ชื่อสมาชิก:<input type="text" name="name"><br>
+        ที่อยู่: <br>
+        <textarea name="address"  cols="40" rows="3"></textarea><br>
+        เบอร์โทร: <input type="text" name="mobile"><br>
+        email: <input type="text" name="email"><br>
+        <input type="submit" value="เพิ่มสมาชิก">
+    </form>
+</body>
+</html>
